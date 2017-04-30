c1ae7b0d33a3a7a91ab212afd8680b459b82f91c
cpenc <chandrahas_p@hotmail.com>
2017-04-29 20:26:57 -0400

    adding run_analysis.R and updated readme.md files

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Getting-and-Cleaning-Data-Course-Project
 Final assignment for Getting and Cleaning Data - Course Project
+
+## What this readme is all about: 
+This readme file only contains details on what is included in the repo and how each of the files are linked with each other.
+
+So it included 3 files
+1. This readme.md
+2. The code for the final assignment of Getting-and-Cleaning-Data-Course-Project
+3. A codebook that explains:
+     a) Everything about the data used of this project
+	 b) Everything about the data generate out of this assignment
+
+Note about the code book: 
+1) As the data for this project is used from a external source, a reference is mentioned to the actual source, if you need to know anything about the source please visit the reference and you should find everything there
+2) As we generate a new tidy data in this assignment out of the actual data, the code book focuses more on this new tidy data and its variables
+
+
+##What does the code for this assignment do:
+Detailed comments are mentioned the code explaining what each line of code does. Please refer to comments in the code to understand code if any code is not understandable in run_analysis.R
+
+But in short this is what the code does:
+ 1. The code needs to have dplyr and tidyr packages installed in R before running the code. install them if you don't already have them!
+ 2. Download the data that is used in this project from: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+ 3. Unzip the downloaded file. The unzip should have "UCI HAR Dataset" folder.
+ 4. "UCI HAR Dataset" folder have all folders(test and train) and files required for this project.
+ 5. In R, set working directory as "UCI HAR Dataset" directory if you want to run "run_analysis.R" code
+ 6. Also save run_analysis.R code file into "UCI HAR Dataset" directory
+ 7. From R or RStudio, after setting working directory as "UCI HAR Dataset" and after having dplyr and tidyr packages installed, run "source("run_analysis.R") to execute the code and geenereate the new tidy data as requested in the assignment
+ 8. So run_analysis.R will do the following:
+   a) Merges the training and the test sets to create one data set.
+   b) Extracts only the measurements on the mean and standard deviation for each measurement.
+   c) Uses descriptive activity names to name the activities in the data set
+   d) Appropriately labels the data set with descriptive variable names.
+   e) From the data set in step e, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+ 9. The run_analysis.R code will generate 1 output file, "FinalTidyDataSet.txt"
+10. FinalTidyDataSet.txt will have the tidy data requested in the assignment
+
---
+++ run_analysis.R
@@ -0,0 +1,90 @@
+#If you intend to run and test this script:
+#You need to have installed dplyr and tidyr packages
+#Run this script in the director "UCI HAR Dataset"
+#to run just set working director as "UCI HAR Dataset" directory and source("run_analysis.R")
+
+
+#loading the required libraries
+library(dplyr)
+library(tidyr)
+
+#Assuming the code will be run from "UCI HAR Dataset" directory 
+#store directory paths in variable to access test and train directories via these variables
+test_data_dir="test/"
+train_data_dir="train/"
+
+#read and load all test data files into R
+X_test<-read.table(paste(test_data_dir,"X_test.txt",sep = ""),header = FALSE)
+y_test<-read.table(paste(test_data_dir,"y_test.txt",sep = ""),header = FALSE)
+subject_test<-read.table(paste(test_data_dir,"subject_test.txt",sep = ""),header = FALSE)
+
+#read and load all train data files into R
+X_train<-read.table(paste(train_data_dir,"X_train.txt",sep = ""),header = FALSE)
+y_train<-read.table(paste(train_data_dir,"y_train.txt",sep = ""),header = FALSE)
+subject_train<-read.table(paste(train_data_dir,"subject_train.txt",sep = ""),header = FALSE)
+
+#read and load the features file into R
+features<-read.table("features.txt",header = FALSE)
+
+#read and load the activity_labels file into R
+activity_labels<-read.table("activity_labels.txt",header = FALSE)
+
+#row bind or join test and train X,y and subject data together by rows
+X_total<-rbind(X_test,X_train)
+y_total<-rbind(y_test,y_train)
+subject_total<-rbind(subject_test,subject_train)
+
+#name columns of all the total data frames:
+#X_total data frame contains all the 561 variables so name their columns from features data frame
+names(X_total)<-tolower(features[,2])
+#y_total data frame contains only the activity_code so name the column as activity_code for ease
+names(y_total)<-"activity_code"
+#subject_total data frame contains only the subjects(range 1 to 30) so name column as subject
+names(subject_total)<-"subject"
+#activity_labels data frame contains 2 columns activity_code and activity_labels so as below:
+names(activity_labels)<-c("activity_code","activity_label")
+
+#club all the 561 variables, subject and activity_code together to form 1 single data set
+OneDataSet<-cbind(subject_total,y_total,X_total)
+#Assignment Step 1 complete!
+
+#Extracts only the measurements on the mean and standard deviation for each measurement.
+#a simple grep will help, need to include strings like mean, sub, subject, activity_code
+#as we only need those columns as per the assignment question 2
+OneNiceDataSet<-tbl_df(OneDataSet[,grep(pattern = 'mean|std|subject|activity_code',
+                                   names(OneDataSet),value = TRUE,ignore.case = TRUE)
+								 ]
+					  ) #<- Assignment Step 2 complete
+
+#Okay, now we need to replace activiiy_codes with activity_labels
+#for this we can just inner join on activity_labels data frame to use the
+#labels in it. After the inner join, we don't need activity_code column 
+#any more, so let us remove it using minus!
+OneNiceDataSet<-inner_join(OneNiceDataSet,activity_labels) %>% select(-activity_code)
+#Assignment Step 3 complete
+
+#Appropriately labels the data set with descriptive variable names.
+#Subsetting the short names with descriptive names and removing unwanted symbols will do!
+k<-names(OneNiceDataSet)                            #storing names into k
+k<-gsub("freq","_frequency",k)                      #replacing freq with _frequency
+k<-gsub("tbody","time_domain_signals_body",k)       #similar for tbody
+k<-gsub("fbody","frequency_domain_signals_body",k)  #similar for fbody
+k<-gsub("tgravity","time_domain_signals_gravity",k) #similar for tgravity
+k<-gsub("acc","_accelerometer_",k)                  #similar for acc
+k<-gsub("gyro","_gyroscope_",k)                     #similar for gyro
+k<-gsub("mag","_magnitude_",k)                      #similar for mag
+k<-gsub("-","_",k)                                  #similar for -
+k<-gsub("__","_",k)                                 #similar for __ (2 underscores)
+#not removing ( and ) as I don't mind having them in the name. As mean is actually mean()
+names(OneNiceDataSet)<-k                            #assign k to names of OneNiceDataSet! done!
+#Assignment Step 4 complete
+
+
+#Independent tidy data set with the average of each variable for each activity and each subject
+#so grouping by subject,activity_label then finding their mean
+FinalTidyDataSet<-group_by(OneNiceDataSet,subject,activity_label) %>% summarise_each(funs(mean))
+
+#writing the grouped means to file.
+write.table(FinalTidyDataSet,file = "FinalTidyDataSet.txt",col.names = TRUE,row.names = FALSE,quote = TRUE)
+#Assignment Step 5 complete
+
